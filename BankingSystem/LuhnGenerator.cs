00ed915c77e012ac938ccd5faf721c3c04779146
Erick Maldonado <erickmg12310@gmail.com>
2026-05-26 00:19:04 -0600

    feat: add Luhn algorithm for card generation and validation

---
+++ BankingSystem/LuhnGenerator.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace BankingSystem;
+
+static class LuhnGenerator
+{
+    
+    
+    public static string GenerateCardNumber()
+    {
+
+        int[] numeros = new int[15];
+        for (int i = 0; i < 15; i++)
+        {
+            numeros[i] = Random.Shared.Next(0,10);
+        }
+        return string.Join("",numeros) + CalculateCheckDigit(numeros);
+
+    }
+    private static int CalculateCheckDigit(int[] digits)
+    {
+        int suma = 0;
+        for (int i = 0; i < 15; i++)
+        {
+            
+            int d = digits[i];
+            if(i % 2 == 0)
+            {
+                d *= 2;
+
+                   if(d > 9)
+                   {
+                    d -= 9;
+                }
+            }
+            suma += d;
+        }
+        return (10 - (suma % 10)) % 10;
+    }
+
+
+    public static bool IsValid(string cardNumber)
+    {
+        int[] digits = cardNumber.Select(c => c - '0').ToArray();
+        int suma = 0;
+        for(int i = 0; i < 16; i++)
+        {
+            int n = digits[i];
+           if( i % 2 == 0)
+            {
+                n *= 2;
+                if(n > 9)
+                {
+                    n -= 9;
+                }
+            }
+            suma += n;
+        }
+        
+        return suma % 10 == 0;
+    }
+   
+}
--- BankingSystem/Program.cs
+++ BankingSystem/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace BankingSystem;
+﻿using System.Runtime.InteropServices;
+
+namespace BankingSystem;
 
 class Program
 {
     static void Main(string[] args)
-    {
+    {  
+        string card = LuhnGenerator.GenerateCardNumber();
+        Console.WriteLine(card);
+        bool IsValid = LuhnGenerator.IsValid(card);
+        Console.WriteLine(IsValid);
         var db = new Database();
         using var connection = db.GetConnection();
         connection.Open();
